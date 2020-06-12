ad206511c75a0690de41f54aa261674dbb0c69b8
Nick Doty <doty.nick@gmail.com>
2020-06-12 00:53:01 -0400

    Replaced discordio with discordjs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -9,20 +9,21 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+const bot = new Discord.Client();
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message === 'bruh') {
-        bot.sendMessage({
-            to: channelID,
-            message: '_bruh_'
-        });
+bot.on('message', (message) => {
+    if (message.content === 'bruh') {
+        message.channel.send('_bruh_');
     }
 });
+
+bot.on('message', (message) => {
+    logger.info(message);
+})
+
+bot.login(auth.token);
