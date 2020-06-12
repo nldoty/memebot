8a6858e8a485b5d6ab082ab2332a7291946e3479
Nick Doty <doty.nick@gmail.com>
2020-06-11 22:28:06 -0400

    First iteration of bruh

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message === 'bruh') {
+        bot.sendMessage({
+            to: channelID,
+            message: '_bruh_'
+        });
+    }
+});
